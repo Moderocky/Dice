5629b1d66d0fbe3f79bfa72f0a5ee719edb7b36e
Moderocky <admin@moderocky.com>
2023-08-30 15:33:31 +0100

    Make rolled objects comparable.

--- src/main/java/mx/kenzie/dice/Rolled.java
+++ src/main/java/mx/kenzie/dice/Rolled.java
@@ -1,5 +1,8 @@
 package mx.kenzie.dice;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
 import java.util.Random;
 
 /**
@@ -7,12 +10,40 @@ import java.util.Random;
  * <p>
  * It's not Rick I promise.
  */
-public interface Rolled {
+public interface Rolled extends Comparable<Rolled>, Serializable {
 
     Random DEFAULT_RANDOM = new Random();
 
+    /**
+     * Compares this Rolled object with another Rolled object.
+     *
+     * @param o the object to be compared
+     * @return 0 if both objects have the same minimum and maximum values,
+     * 1 if this object has greater minimum and maximum values,
+     * -1 if this object has smaller minimum and maximum values,
+     * a comparison of the average value of the rolls of each object.
+     */
+    @Override
+    default int compareTo(@NotNull Rolled o) {
+        if (this.min() == o.min() && this.max() == o.max()) return 0;
+        if (this.min() >= o.min() && this.max() >= o.max()) return 1;
+        if (this.min() <= o.min() && this.max() <= o.max()) return -1;
+        return Integer.compare(this.min() + (this.range() / 2), o.min() + (o.range() / 2));
+    }
+
     default int roll() {
         return this.roll(Rolled.DEFAULT_RANDOM);
+    }
+
+    /**
+     * A faster rolling method that produces a value between the minimum and maximum, ignoring biases.
+     * Generates a random number between the min and max values (inclusive)
+     * using the default random source.
+     *
+     * @return An evenly-distributed random roll between the min and max values.
+     */
+    default int approximateRoll() {
+        return DEFAULT_RANDOM.nextInt(this.min(), this.max() + 1);
     }
 
     default Rolled advantage() {
