fea9027f746042f98d3ee302b45a957b85bb900e
Moderocky <admin@moderocky.com>
2023-12-02 10:11:11 +0000

    Convert bonus to number.

--- src/main/java/mx/kenzie/dice/Bonus.java
+++ src/main/java/mx/kenzie/dice/Bonus.java
@@ -1,5 +1,6 @@
 package mx.kenzie.dice;
 
+import java.io.Serial;
 import java.util.Random;
 
 /**
@@ -8,56 +9,86 @@ import java.util.Random;
  * <p>
  * Specifically used for score generation, such as "2d6+5".
  */
-public record Bonus(int value) implements Rolled {
-
+public class Bonus extends Number implements Rolled {
+    
+    @Serial
+    private static final long serialVersionUID = 0L;
+    private final int value;
+    
+    public Bonus(int value) {this.value = value;}
+    
     public Bonus(String counter) {
         this(Integer.parseInt(counter));
     }
-
+    
     @Override
     public int roll(Random random) {
         return value;
     }
-
+    
     @Override
     public int roll() {
         return value;
     }
-
+    
     @Override
     public int min() {
         return value;
     }
-
+    
     @Override
     public int max() {
         return value;
     }
-
+    
     @Override
     public int range() {
         return 1;
     }
-
+    
     public Dice toDice() {
         return new Dice(value, 1);
     }
-
+    
     @Override
     public boolean equals(Object object) {
         if (this == object) return true;
         if (!(object instanceof final Bonus bonus)) return false;
         return value == bonus.value;
     }
-
+    
     @Override
     public int hashCode() {
         return value;
     }
-
+    
     @Override
     public String toString() {
         return Integer.toString(value);
     }
-
+    
+    public int value() {
+        return value;
+    }
+    
+    @Override
+    public int intValue() {
+        return value;
+    }
+    
+    @Override
+    public long longValue() {
+        return value;
+    }
+    
+    @Override
+    public float floatValue() {
+        return value;
+    }
+    
+    @Override
+    public double doubleValue() {
+        return value;
+    }
+    
 }
